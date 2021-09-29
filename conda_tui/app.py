d641c38fd3e4804b8661bfccc6ee871d3b753f70
Matt Kramer <mkramer@anaconda.com>
2021-09-29 10:02:39 -0500

    Add basic grid layout

--- conda_tui/app.py
+++ conda_tui/app.py
@@ -1,10 +1,13 @@
 from pathlib import Path
 
+from rich.table import Table
 from rich.text import Text
 from textual.app import App
 from textual.widgets import Footer
 from textual.widgets import Header
 from textual.widgets import ScrollView
+
+from conda_tui.environment import list_environments
 
 HERE = Path(__file__).parent
 
@@ -25,16 +28,42 @@ class CondaTUI(App):
     async def on_mount(self) -> None:
         """Call after terminal goes in to application mode"""
 
-        # Create our widgets
+        grid = await self.view.dock_grid()
+
+        grid.add_column(fraction=1, name="left", min_size=20)
+        grid.add_column(fraction=3, name="right")
+
+        # TODO: I can't seem to get the main row to expand to full-screen when I set max_size
+        #       of header and footer. Maybe a Textual bug we can fix and PR?
+        #       To reproduce, remove 'min_size' argument from the main row.
+        grid.add_row(name="header", max_size=3)
+        grid.add_row(name="main", min_size=50)
+        grid.add_row(name="footer", max_size=1)
+
+        grid.add_areas(
+            header="left-start|right-end,header",
+            env_list="left,main",
+            package_list="right,main",
+            footer="left-start|right-end,footer",
+        )
+
+        table = Table("Name", "Path", title="Environments")
+        for env in list_environments():
+            table.add_row(env.name, env.path)
+
+        environment_list = ScrollView(table)
+
+        # Display the package list
+        # TODO: Should toggle between logo and list when environment is selected
         text = Text(LOGO_TEXT, style="green", justify="left")
-        self.body = ScrollView(text)
+        package_list = ScrollView(text)
 
-        # Dock our widgets
-        await self.view.dock(Header(), edge="top")
-        await self.view.dock(Footer(), edge="bottom")
-
-        # Dock the logo body view
-        await self.view.dock(self.body, edge="top")
+        grid.place(
+            header=Header(),
+            env_list=environment_list,
+            package_list=package_list,
+            footer=Footer(),
+        )
 
 
 def run() -> None:
