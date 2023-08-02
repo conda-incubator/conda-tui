bc8fa0ca9679541dd7537d4c2479148704663279
mattkram <mkramer@anaconda.com>
2023-08-02 14:06:11 -0500

    Refactor to use Screen API

--- src/conda_tui/app.py
+++ src/conda_tui/app.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 from textual.app import App
 from textual.app import ComposeResult
-from textual.reactive import reactive
+from textual.screen import Screen as _Screen
 
 from conda_tui.widgets import EnvironmentList
 from conda_tui.widgets import Footer
@@ -16,57 +16,50 @@ from conda_tui.widgets import Logo
 # from conda_tui.table import PackageTableWidget
 
 
+class Screen(_Screen):
+    def compose(self) -> ComposeResult:
+        yield Header(show_clock=True)
+        yield Footer()
+
+
+class HomeScreen(Screen):
+    def compose(self) -> ComposeResult:
+        yield from super().compose()
+        yield Logo(id="logo")
+
+
+class EnvironmentScreen(Screen):
+    def compose(self) -> ComposeResult:
+        yield from super().compose()
+        yield EnvironmentList(id="environment-list")
+
+
 class CondaTUI(App):
     """A hacked-together Conda Text User Interface (TUI)."""
 
     TITLE = "conda-tui"
     CSS_PATH = Path("styles.css")
+    SCREENS = {
+        "home": HomeScreen(),
+        "environments": EnvironmentScreen(),
+    }
     BINDINGS = [
-        ("h", "go_home", "Home"),
-        ("e", "show_environment_list", "Environments"),
+        ("h", "switch_screen('home')", "Home"),
+        ("e", "switch_screen('environments')", "Environments"),
         ("q", "quit", "Quit"),
     ]
 
-    show_logo = reactive(True)
-    show_environment_list = reactive(False)
+    def on_mount(self) -> None:
+        """When we start up, push the home screen.
 
-    def compose(self) -> ComposeResult:
-        yield Header(show_clock=True)
-        yield Logo(id="logo")
-        yield EnvironmentList(classes="hidden", id="environment-list")
-        yield Footer()
+        This allows us to use switch_screen to switch between the various views without
+        having an infinitely-growing screen stack.
 
-    def action_go_home(self) -> None:
-        self.show_logo = True
-        self.show_environment_list = False
-
-    def action_show_environment_list(self) -> None:
-        self.show_logo = False
-        self.show_environment_list = True
-
-    def watch_show_logo(self, show_logo: bool) -> None:
-        """Hide or un-hide logo based on show_logo reactive variable."""
-        logo = self.query_one("#logo")
-        if show_logo:
-            logo.remove_class("hidden")
-        else:
-            logo.add_class("hidden")
-
-    def watch_show_environment_list(self) -> None:
-        """Hide or un-hide environment list based on show_environment_list reactive variable."""
-        env_list = self.query_one("#environment-list")
-        if self.show_environment_list:
-            env_list.remove_class("hidden")
-        else:
-            env_list.add_class("hidden")
+        """
+        self.push_screen("home")
 
     # async def on_mount(self) -> None:
     #     """Call after terminal goes in to application mode."""
-    #
-    #     self.environment_list = ScrollView(EnvironmentTree())
-    #     await self.view.dock(
-    #         self.environment_list, edge="left", size=30, name="sidebar"
-    #     )
     #
     #     self.package_list = ScrollView(get_logo())
     #     await self.view.dock(self.package_list, edge="right")
