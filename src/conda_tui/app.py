6cafeda3a248708fc8aa71a3efd6ed8c8c3074d4
mattkram <mkramer@anaconda.com>
2023-08-02 22:39:11 -0500

    Move screens to own module

--- src/conda_tui/app.py
+++ src/conda_tui/app.py
@@ -1,58 +1,10 @@
 from pathlib import Path
-from typing import Optional
 
 from textual.app import App
-from textual.app import ComposeResult
-from textual.reactive import reactive
-from textual.screen import Screen as _Screen
 
-from conda_tui.environment import Environment
-from conda_tui.widgets import EnvironmentList
-from conda_tui.widgets import Footer
-from conda_tui.widgets import Header
-from conda_tui.widgets import Logo
-from conda_tui.widgets.table import PackageTableWidget
-
-
-class Screen(_Screen):
-    def compose(self) -> ComposeResult:
-        yield Header(show_clock=True)
-        yield Footer()
-
-
-class HomeScreen(Screen):
-    def compose(self) -> ComposeResult:
-        yield from super().compose()
-        yield Logo(id="logo")
-
-
-class EnvironmentScreen(Screen):
-    def compose(self) -> ComposeResult:
-        yield from super().compose()
-        yield EnvironmentList(id="environment-list")
-
-
-class PackageListScreen(Screen):
-    """A screen to display the packages installed into a specific environment."""
-
-    environment = reactive[Optional[Environment]](None)
-
-    BINDINGS = [
-        ("escape", "go_back", "Back"),
-    ]
-
-    def compose(self) -> ComposeResult:
-        yield from super().compose()
-        # TODO: This is just placeholder text for now
-        from rich.text import Text
-        from textual.widgets import Static
-
-        if self.environment is not None:
-            yield Static(Text(str(self.environment.prefix)))
-            yield PackageTableWidget(self.environment)
-
-    def action_go_back(self) -> None:
-        self.dismiss()
+from conda_tui.screens import EnvironmentScreen
+from conda_tui.screens import HomeScreen
+from conda_tui.screens import PackageListScreen
 
 
 class CondaTUI(App):
