da99853fa880a46b813c356618c0893d72bdb5c6
mattkram <mkramer@anaconda.com>
2023-08-02 23:27:44 -0500

    Use DataTable instead of custom widget

--- src/conda_tui/screens.py
+++ src/conda_tui/screens.py
@@ -3,13 +3,14 @@ from typing import Optional
 from textual.app import ComposeResult
 from textual.reactive import reactive
 from textual.screen import Screen as _Screen
+from textual.widgets import DataTable
 
 from conda_tui.environment import Environment
+from conda_tui.package import list_packages_for_environment
 from conda_tui.widgets import EnvironmentList
 from conda_tui.widgets import Footer
 from conda_tui.widgets import Header
 from conda_tui.widgets import Logo
-from conda_tui.widgets.table import PackageTableWidget
 
 
 class Screen(_Screen):
@@ -42,7 +43,23 @@ class PackageListScreen(Screen):
     def compose(self) -> ComposeResult:
         yield from super().compose()
         assert self.environment is not None, "Shouldn't be possible"
-        yield PackageTableWidget(self.environment)
+        yield DataTable()
+
+    def on_mount(self) -> None:
+        """Load the packages for the environment and populate the table."""
+        table = self.query_one(DataTable)
+        table.cursor_type = "row"
+        table.add_columns("Name", "Description", "Version", "Build", "Channel")
+        packages = list_packages_for_environment(self.environment)
+        for row_num, pkg in enumerate(packages):
+            table.add_row(
+                pkg.name,
+                pkg.description,
+                pkg.status,
+                pkg.build,
+                pkg.schannel,
+                key=pkg.name,
+            )
 
     def action_go_back(self) -> None:
         self.dismiss()
