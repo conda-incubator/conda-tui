0150d0f97707a6ae9b103cfedf73d0acb2c83596
mattkram <mkramer@anaconda.com>
2023-08-02 22:08:03 -0500

    Begin to wire up package list view

--- src/conda_tui/app.py
+++ src/conda_tui/app.py
@@ -1,9 +1,12 @@
 from pathlib import Path
+from typing import Optional
 
 from textual.app import App
 from textual.app import ComposeResult
+from textual.reactive import reactive
 from textual.screen import Screen as _Screen
 
+from conda_tui.environment import Environment
 from conda_tui.widgets import EnvironmentList
 from conda_tui.widgets import Footer
 from conda_tui.widgets import Header
@@ -34,6 +37,28 @@ class EnvironmentScreen(Screen):
         yield EnvironmentList(id="environment-list")
 
 
+class PackageListScreen(Screen):
+    """A screen to display the packages installed into a specific environment."""
+
+    environment = reactive[Optional[Environment]](None)
+
+    BINDINGS = [
+        ("escape", "go_back", "Back"),
+    ]
+
+    def compose(self) -> ComposeResult:
+        yield from super().compose()
+        # TODO: This is just placeholder text for now
+        from rich.text import Text
+        from textual.widgets import Static
+
+        if self.environment is not None:
+            yield Static(Text(str(self.environment.prefix)))
+
+    def action_go_back(self) -> None:
+        self.dismiss()
+
+
 class CondaTUI(App):
     """A hacked-together Conda Text User Interface (TUI)."""
 
@@ -42,6 +67,7 @@ class CondaTUI(App):
     SCREENS = {
         "home": HomeScreen(),
         "environments": EnvironmentScreen(),
+        "package_list": PackageListScreen(),
     }
     BINDINGS = [
         ("h", "switch_screen('home')", "Home"),
