5fad337283f86363b63c6803d4813b43ec845e9d
mattkram <mkramer@anaconda.com>
2023-08-02 22:37:13 -0500

    Clean up app.py

--- src/conda_tui/app.py
+++ src/conda_tui/app.py
@@ -11,12 +11,7 @@ from conda_tui.widgets import EnvironmentList
 from conda_tui.widgets import Footer
 from conda_tui.widgets import Header
 from conda_tui.widgets import Logo
-
-# from textual.widgets._tree_control import TreeNode, TreeControl
-# from textual.scroll_view import ScrollView
-
-# from conda_tui.package import list_packages_for_environment
-# from conda_tui.table import PackageTableWidget
+from conda_tui.widgets.table import PackageTableWidget
 
 
 class Screen(_Screen):
@@ -54,6 +49,7 @@ class PackageListScreen(Screen):
 
         if self.environment is not None:
             yield Static(Text(str(self.environment.prefix)))
+            yield PackageTableWidget(self.environment)
 
     def action_go_back(self) -> None:
         self.dismiss()
@@ -83,30 +79,6 @@ class CondaTUI(App):
 
         """
         self.push_screen("home")
-
-    # async def on_mount(self) -> None:
-    #     """Call after terminal goes in to application mode."""
-    #
-    #     self.package_list = ScrollView(get_logo())
-    #     await self.view.dock(self.package_list, edge="right")
-    #
-    # async def handle_tree_click(self, message) -> None:
-    #     """Display the package list if the environment exists."""
-    #     if not message.node.data.path:
-    #         return
-    #
-    #     # if not message.node.loaded:
-    #     await self.show_package_table(message.node)
-    #     await message.node.expand()
-    #
-    # async def action_display_logo(self) -> None:
-    #     """Display the logo when "H" is pressed."""
-    #     await self.package_list.update(get_logo())
-
-    # async def show_package_table(self, node: TreeNode[Environment]) -> None:
-    #     """Update the package list table based on selected environment."""
-    #     packages = list_packages_for_environment(node.data)
-    #     await self.package_list.update(PackageTableWidget(node.data, packages))
 
 
 def run() -> None:
